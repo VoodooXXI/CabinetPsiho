bfb75008d24772807fa687a955836cf13bb68306
VoodooXXI <118216253+VoodooXXI@users.noreply.github.com>
2023-08-23 09:13:38 +0300

    Cabinet psiho
    Setup cabinet psiho

---
+++ abinetsiho/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello Webpage</h1>
+    </div>
+  );
+}
+
+export default App;
